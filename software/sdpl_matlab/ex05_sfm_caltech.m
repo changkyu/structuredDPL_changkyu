68e8ecdc2f9c0fa4f5a3e0e9d35009057929458a
changkyu <changkyusong86@gmail.com>
2015-02-01 15:45:39 -0500

    show result

--- software/sdpl_matlab/ex05_sfm_caltech.m
+++ software/sdpl_matlab/ex05_sfm_caltech.m
@@ -11,6 +11,8 @@
 %
 %--------------------------------------------------------------------------
 
+env_setting();
+
 %% Experiments Description
 models_desc = { 
                 struct('name','cppca', 'type','SVD',   'fn',@cppca   ,'skip',0), ...
@@ -36,11 +38,11 @@ format_mat_result_sdppca = '%s_n%03d_r%03d_r%03d_%s.mat';
 objfreq_c = 0;
 objfreq_d = 0;
 
-load('data/caltech_turntable/data.mat');
+load('../../dataset/caltech_turntable/data.mat');
 measurement_matrices = {BallSander, BoxStuff, Rooster, Standing, StorageBin};
 datastr = {'BallSander', 'BoxStuff', 'Rooster', 'Standing', 'StorageBin'};
 
-repeats = 20;
+repeats = 1;%20;
 SAs = zeros(length(datastr), 5, repeats);
 SAs_sdppca = zeros(length(datastr), repeats, length(idxes_model_dm));
 
@@ -121,7 +123,7 @@ for idm = 1:length(measurement_matrices)
 
         % D-PPCA
         V = get_sample_assign(J, D, 1); % node assignment of samples(=frames)
-        E = get_adj_graph(J); E = E{2}; % graph topology (1 = compete, 2 = ring)
+        E = get_adj_graph(J); E = E{1}; % graph topology (1 = compete, 2 = ring)
         cm4 = dppca_t(mm_trans_t, V, E, M, ETA, THRESHd, m_init4, objfreq_d);
 
         % Distributed settting result: we report maximum (worst) angle
@@ -138,6 +140,7 @@ for idm = 1:length(measurement_matrices)
             cm4, max_angle);
         
         % SPPCA
+        model_dm = cell(length(idxes_model_dm),1);
         for idx_model = idxes_model_dm
             model_desc = models_desc{idx_model};
             % result directory
@@ -155,22 +158,22 @@ for idm = 1:length(measurement_matrices)
             path_result = [dir_result '/' mat_result];
 
             if isequal(model_desc.fn, @dppca)
-                model_dm = model_desc.fn(mm_trans_t, V, E, M, ETA, THRESHd, m_init4);
+                model_dm{idx_model - idxes_model_dm(1) + 1} = model_desc.fn(mm_trans_t, V, E, M, ETA, THRESHd, m_init4);
             elseif isequal(model_desc.fn, @sdppca)
-                model_dm = model_desc.fn(mm_trans_t, V, E, M, ETA, model_desc.type, THRESHd, m_init4, 10000, 'fix_mu');
+                model_dm{idx_model - idxes_model_dm(1) + 1} = model_desc.fn(mm_trans_t, V, E, M, ETA, model_desc.type, THRESHd, m_init4, 10000, 'fix_mu');
             else
                 error(['[Error] Undefined Function - ' model_desc.name]);
             end
 
             % Distributed settting result
-            max_angle = subspace(GT, model_dm.W(:,:,1));
+            max_angle = subspace(GT, model_dm{idx_model - idxes_model_dm(1) + 1}.W(:,:,1));
             for idj = 2:J
-                if max_angle < subspace(GT, model_dm.W(:,:,idj))
-                    max_angle = subspace(GT, model_dm.W(:,:,idj));
+                if max_angle < subspace(GT, model_dm{idx_model - idxes_model_dm(1) + 1}.W(:,:,idj))
+                    max_angle = subspace(GT, model_dm{idx_model - idxes_model_dm(1) + 1}.W(:,:,idj));
                 end
             end
 
-            save_parfor_model_init_angle(path_result, model_dm, m_init4, max_angle);    
+            save_parfor_model_init_angle(path_result, model_dm{idx_model - idxes_model_dm(1) + 1}, m_init4, max_angle);    
         end
 
     end % parfor: 20 independent runs (initial value)
@@ -184,7 +187,7 @@ for idm = 1:length(measurement_matrices)
         SAs(idm, 4, idr2) = subspace(GT, cm3.W);
         load(sprintf('result/sfm_real_caltech/tmp_%03d_%03d.mat', idm, idr2));
         SAs(idm, 5, idr2) = cm_init;
-        delete(sprintf('result/sfm_real_caltech/tmp_%03d_%03d.mat', idm, idr2));
+%         delete(sprintf('result/sfm_real_caltech/tmp_%03d_%03d.mat', idm, idr2));
         
         for idx_model = idxes_model_dm     
             model_desc = models_desc{idx_model};
@@ -196,9 +199,9 @@ for idm = 1:length(measurement_matrices)
 
             % result mat file
             if strcmp(model_desc.name,'sdppca')
-                mat_result = sprintf(format_mat_result_sdppca, model_desc.name, idn, idr, idr2, model_desc.type);
+                mat_result = sprintf(format_mat_result_sdppca, model_desc.name, idm, idr2, model_desc.type);
             else
-                mat_result = sprintf(format_mat_result_dppca, model_desc.name, idn, idr, idr2);
+                mat_result = sprintf(format_mat_result_dppca, model_desc.name, idm, idr2);
             end
             path_result = [dir_result '/' mat_result];
 
@@ -212,15 +215,23 @@ for idm = 1:length(measurement_matrices)
     fprintf('cm4: (1) GT and (2) D-PPCA                : %.15f (Mean of %d runs)\n', ...
         tmp(idm, 5), repeats);
     
+    tmp = mean(SAs_sdppca, 2);
+    for idx_model = idxes_model_dm     
+        fprintf('cm4: (1) GT and (2) SD-PPCA               : %.15f (Mean of %d runs)\n', ...
+        tmp(idm, idx_model - idxes_model_dm(1) + 1), repeats);
+    end
+    
     %--------------------------------------------------------------
     % Save the model for future usage
     ms_path = sprintf('result/sfm_real_caltech/result_%s.mat', datastr{idm});
-    save(ms_path, 'sfm_M', 'sfm_S', 'sfm_b', 'cm2', 'cm3');
+%     save(ms_path, 'sfm_M', 'sfm_S', 'sfm_b', 'cm2', 'cm3');
+    save(ms_path, 'sfm_M', 'sfm_S', 'sfm_b', 'cm2', 'cm3', 'cm4', 'model_dm');
 end
 
-clearvars -except SAs;
-
 save('result/sfm_real_caltech/result_sfm_caltech_all.mat','SAs');
+save('result/sfm_real_caltech/result_sfm_caltech_sdppca_all.mat','SAs_sdppca');
+
+clearvars -except SAs;
 
 % In order to see these, you need to save cm4 above separately as 'dppca_*'
 %show_result_4_caltech;
